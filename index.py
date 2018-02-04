166518479f1b58533debc7856267f8600b0333d2
yong hao <yonghao.choo@gmail.com>
2018-02-04 15:05:38 +0800

    added feature: convert ogg to wav

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from wit import Wit
-from tokenconfig import TOKEN, WIT
+from tokenconfig import TOKEN, WIT, CONVERT
 import cloudconvert
 import logging
 
@@ -9,6 +9,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 client = Wit(WIT)
 updater = Updater(token=TOKEN)
+api = cloudconvert.Api()
 
 dispatcher = updater.dispatcher
 
@@ -18,5 +19,26 @@ def start(bot, update):
 #Start function Commandhandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+def convertOgg():
+    voice = update.message.voice.file_id
+
+    process = api.convert({
+        "inputformat": "ogg",
+        "outputformat": "wav",
+        "input": "raw",
+        "file": voice,
+        "filename": "voice.ogg"
+    })
+    process.wait()
+    return process
+
+
+def overall(bot, update):
+    # convert function
+    # wit.ai voice to text function
+
+overall_handler = MessageHandler(Filters.voice, overall)
+dispatcher.add_handler(overall_handler)
 
 updater.start_polling()
